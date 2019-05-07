663ee0b5e537820f30a15c7c7ee6b02643195e86
350509838@qq.com <8221630dr>
2019-05-07 11:40:14 +0800

    1

---
+++ src/main/java/com/dr/spider/parser/DemoManualNewsCrawler.java
@@ -0,0 +1,55 @@
+package com.dr.spider.parser;
+
+import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
+import cn.edu.hfut.dmic.webcollector.model.Links;
+import cn.edu.hfut.dmic.webcollector.model.Page;
+import cn.edu.hfut.dmic.webcollector.plugin.rocks.BreadthCrawler;
+import org.jsoup.select.Elements;
+
+public class DemoManualNewsCrawler extends BreadthCrawler {
+
+  public DemoManualNewsCrawler(String crawlPath, boolean autoParse) {
+    super(crawlPath, autoParse);
+    // add 5 start pages and set their type to "list"
+    //"list" is not a reserved word, you can use other string instead
+    this.addSeedAndReturn("https://popjav.tv/").type("list");
+    for (int pageIndex = 2; pageIndex <= 5; pageIndex++) {
+      String seedUrl = String.format("https://popjav.tv/page/%d", pageIndex);
+      this.addSeed(seedUrl, "list");
+    }
+    setThreads(50);
+    getConf().setTopN(100);
+
+    //enable resumable mode
+    //setResumable(true);
+  }
+
+  @Override
+  public void visit(Page page, CrawlDatums crawlDatums) {
+    if (page.code() == 301 || page.code() == 302) {
+      crawlDatums.addAndReturn(page.location()).meta(page.meta());
+      return;
+    }
+    String url = page.url();
+    if (page.matchType("list")) {
+      /*if type is "list"*/
+      /*detect content page by css selector and mark their types as "content"*/
+      crawlDatums.add(page.links(".video>a")).type("content");
+    } else if (page.matchType("content")) {
+
+      System.out.println(url);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    DemoManualNewsCrawler crawler = new DemoManualNewsCrawler("crawl", false);
+
+    crawler.getConf().setExecuteInterval(5000);
+
+    crawler.getConf().set("title_prefix", "PREFIX_");
+    crawler.getConf().set("content_length_limit", 20);
+
+    /*start crawl with depth of 4*/
+    crawler.start(4);
+  }
+}
