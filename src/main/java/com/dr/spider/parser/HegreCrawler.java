c7451ac21648d0ba6bbb6c8507eaa318c81c35e6
350509838@qq.com <8221630dr>
2019-05-11 11:10:23 +0800

    1

--- src/main/java/com/dr/spider/parser/HegreCrawler.java
+++ src/main/java/com/dr/spider/parser/HegreCrawler.java
@@ -19,7 +19,7 @@ import org.bson.Document;
 
 public class HegreCrawler extends BreadthCrawler {
 
-  public static final String cookie = "_retina=0; _ga=GA1.2.411007182.1556615059; __auc=4beda20a16a6d7cd8ccac919d9e; stay-in-touch=0; _width=2560; __asc=84cae5f616a9bce0e206f87ccf8; _gid=GA1.2.127777201.1557392134; locale=en; login=ODljZjJkMGZjYzY1YWM2OWE4YzcwMDM4YjA4NGRmZDd8MTU1NzM5MjI0M3wzfDMwNDM3NnwyMDIuNDYuMzcuMTc2fDA=; _www.hegre.com_session=BAh7CDoPc2Vzc2lvbl9pZEkiJWI0YzQ4YTRmOTA5NTk0NDgzY2UyOWQ0OWQwZWM0MzM0BjoGRUY6EF9jc3JmX3Rva2VuSSIxeW5mblQ3R0FGczFHZE5vdUxzU3Q1eXA0Z0UvRWtDNjNYb2xsc0Yya0oyUT0GOwZGSSIKZmxhc2gGOwZUSUM6J0FjdGlvbkNvbnRyb2xsZXI6OkZsYXNoOjpGbGFzaEhhc2h7Bjogc2V0X2NvbGxlY3Rpb25fcG9wdXBfY29va2llVAY6CkB1c2VkewY7CUY%3D--fe2ac89ec34b53132c6bda23bedfebd5037b1616";
+  public static final String cookie = "_retina=0; _ga=GA1.2.411007182.1556615059; __auc=4beda20a16a6d7cd8ccac919d9e; stay-in-touch=0; _width=2560; locale=en; __asc=49abbdac16aa4db68c3a18f97f7; _gid=GA1.2.1453415963.1557544004; _gat_betaVisitorsTracker=1; _gat_betaNonMembersTracker=1; login=M2QxOTlkYTQwMzUxNDNiNDNjNTg3ODliZWY1ODBiODR8MTU1NzU0NDA0N3wzfDMwNDM3NnwyMDIuNDYuMzcuMTc2fDA=; _www.hegre.com_session=BAh7CDoPc2Vzc2lvbl9pZEkiJWI0YzQ4YTRmOTA5NTk0NDgzY2UyOWQ0OWQwZWM0MzM0BjoGRUY6EF9jc3JmX3Rva2VuSSIxeW5mblQ3R0FGczFHZE5vdUxzU3Q1eXA0Z0UvRWtDNjNYb2xsc0Yya0oyUT0GOwZGSSIKZmxhc2gGOwZUSUM6J0FjdGlvbkNvbnRyb2xsZXI6OkZsYXNoOjpGbGFzaEhhc2h7Bjogc2V0X2NvbGxlY3Rpb25fcG9wdXBfY29va2llVAY6CkB1c2VkewY7CUY%3D--fe2ac89ec34b53132c6bda23bedfebd5037b1616";
 
   public HegreCrawler(String crawlPath, boolean autoParse) {
     super(crawlPath, autoParse);
@@ -29,7 +29,7 @@ public class HegreCrawler extends BreadthCrawler {
       this.addSeed(seedUrl, "list");
     }
 
-    setThreads(50);
+    setThreads(2);
     getConf().setTopN(100);
 
   }
@@ -74,7 +74,7 @@ public class HegreCrawler extends BreadthCrawler {
     crawler.getConf().setExecuteInterval(5000);
     crawler.getConf().set("title_prefix", "PREFIX_");
     crawler.getConf().set("content_length_limit", 20);
-    crawler.getConf().setThreadKiller(1000 * 60 * 60 * 2);
+    crawler.getConf().setThreadKiller(1000 * 60 * 60 * 24);
     /*start crawl with depth of 4*/
     crawler.start(4);
   }
